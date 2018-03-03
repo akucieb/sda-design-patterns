dd33c083354a712872e8fcf9345523ef2898b855
Anita Kucieba <akucieba@gmail.com>
2018-03-03 15:06:42 +0100

    add factory design patern

--- src/main/java/pl/sda/poznan/factory/App.java
+++ src/main/java/pl/sda/poznan/factory/App.java
@@ -5,18 +5,30 @@ import pl.sda.poznan.factory.model.Employee;
 import pl.sda.poznan.factory.writer.CompanyWriter;
 import pl.sda.poznan.factory.writer.TxtCompanyWriter;
 
+import java.util.Scanner;
+
 
 public class App {
     public static void main(String[] args) {
-        Employee director = new Employee("Jan","Kowalski","jankowalski@gmail.com",2554037.25);
-        Employee menager = new Employee("Marcin","Nowak","nowakm@gmail.com",154037.25);
+        Employee director = new Employee("Jan", "Kowalski", "jankowalski@gmail.com", 2554037.25);
+        Employee menager = new Employee("Marcin", "Nowak", "nowakm@gmail.com", 154037.25);
 
         Company company = new Company();
         company.addEmployee(director);
         company.addEmployee(menager);
 
-       CompanyWriter writer = new TxtCompanyWriter("firma.txt");
-       writer.write(company);
-       }
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Podaj nazwe pliku, do ktorego chcesz zapisac:");
+
+        String filename = scanner.next();
+        CompanyWriterFactory companyWriterFactory = new CompanyWriterFactory();
+
+        try {
+            CompanyWriter writer = companyWriterFactory.create(filename);
+            writer.write(company);
+        } catch (IllegalArgumentException ex) {
+            System.out.println(ex.getMessage());
+        }
     }
+}
 
